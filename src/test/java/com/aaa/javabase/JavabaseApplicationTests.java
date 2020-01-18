1a69b1f4654025504faa1f85b2eea364b6a448e0
tianliuzhen <tianliuzhen@haoxiaec.com>
2020-01-18 10:28:32 +0800

    first commit

---
+++ src/test/java/com/aaa/javabase/JavabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaa.javabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
