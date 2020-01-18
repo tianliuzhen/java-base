773ba54bdf66a9b90be84da0488be9e2e834b06d
tianliuzhen <tianliuzhen@haoxiaec.com>
2020-01-18 14:51:22 +0800

    面向对象 之 多态

---
+++ src/main/java/com/aaa/javabase/objectOriented/polymorphism/Employee.java
@@ -0,0 +1,46 @@
+package com.aaa.javabase.objectOriented.polymorphism;
+
+/**
+ * description: 描述
+ *
+ * @author 田留振(liuzhen.tian @ haoxiaec.com)
+ * @version 1.0
+ * @date 2020/1/18
+ */
+public abstract class Employee {
+    private String name;
+    private String address;
+    private int number;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String address, int number) {
+        System.out.println("Employee 构造函数");
+        this.name = name;
+        this.address = address;
+        this.number = number;
+    }
+    public abstract double computePay();
+
+    public void mailCheck() {
+        System.out.println("邮寄支票给： " + this.name
+                + " " + this.address);
+    }
+    @Override
+    public String toString() {
+        return name + " " + address + " " + number;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String newAddress) {
+        address = newAddress;
+    }
+    public int getNumber() {
+        return number;
+    }
+}
---
+++ src/main/java/com/aaa/javabase/objectOriented/polymorphism/Tom.java
@@ -0,0 +1,14 @@
+package com.aaa.javabase.objectOriented.polymorphism;
+
+/**
+ * description: 描述
+ *
+ * @author 田留振(liuzhen.tian @ haoxiaec.com)
+ * @version 1.0
+ * @date 2020/1/18
+ */
+public class Tom extends Employee {
+    public Tom(String name, String address, int number) {
+        super(name, address, number);
+    }
+}
