9c1b815ada441082d66809bd0a7e8fbe811d4f68
tianliuzhen <746033097@qq.com>
2020-02-14 20:53:49 +0800

    原型模式（含深拷贝和浅拷贝）其实就是引用传递和值传递

---
+++ src/main/java/com/aaa/javabase/designpattern/prototype/improve/Client.java
@@ -0,0 +1,24 @@
+package com.aaa.javabase.designpattern.prototype.improve;
+
+/**
+ * description: 描述
+ * @author 田留振(liuzhen.tian @ haoxiaec.com)
+ * @version 1.0
+ * @date 2020/2/14
+ */
+public class Client {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        System.out.println("测试原型创建");
+        Sheep sheep = new Sheep("duoli", 99, "白色");
+        sheep.friend = new Sheep("朋友", 25, "灰色");
+        Sheep sheep1 = (Sheep) sheep.clone();
+        Sheep sheep2 = (Sheep) sheep.clone();
+        Sheep sheep3 = (Sheep) sheep.clone();
+        System.out.println("sheep1="+sheep1+"sheep1.hashcode= "+sheep1.hashCode()+" sheep1.friend.hashCode= "+sheep1.friend.hashCode());
+        System.out.println("sheep2="+sheep2+"sheep1.hashcode= "+sheep2.hashCode()+" sheep2.friend.hashCode= "+sheep2.friend.hashCode());
+        System.out.println("sheep3="+sheep3+"sheep1.hashcode= "+sheep3.hashCode()+" sheep3.friend.hashCode=  "+sheep3.friend.hashCode());
+        System.out.println(sheep1 == sheep2);
+        System.out.println("-----------------------");
+
+    }
+}
---
+++ src/main/java/com/aaa/javabase/designpattern/prototype/improve/Sheep.java
@@ -0,0 +1,65 @@
+package com.aaa.javabase.designpattern.prototype.improve;
+
+/**
+ * description: 原型模式创建的对象是一个（模板）对象
+ *  spring 创建 bean 用到 单例和多例
+ *
+ * @author 田留振(liuzhen.tian @ haoxiaec.com)
+ * @version 1.0
+ * @date 2020/2/14
+ */
+public class Sheep implements Cloneable {
+    private String name;
+    private int age;
+    private String color;
+    public Sheep friend;
+
+    public Sheep(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Sheep{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    /**
+     * 克隆该实例，使用默认的clone() 方法完成
+     */
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Sheep sheep=null;
+        sheep= (Sheep) super.clone();
+        return sheep;
+    }
+}
