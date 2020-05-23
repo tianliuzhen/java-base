b4f29ef7bd852ca5e90cf0f7d61e51882ffa4b8a
tianliuzhen <tlzallen@163.com>
2020-05-23 22:55:13 +0800

    stream 分组求和统计先转map再转list

---
+++ src/main/java/com/aaa/javabase/stream/groupby/Stu.java
@@ -0,0 +1,20 @@
+package com.aaa.javabase.stream.groupby;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * description: 描述
+ *
+ * @author 田留振(liuzhen.tian @ haoxiaec.com)
+ * @version 1.0
+ * @date 2020/2/15
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Stu {
+    private String name;
+    private Long age;
+}
