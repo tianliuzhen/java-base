c8e2ea59c5de65e0da17f9e98b6478015c5a080e
tianliuzhen <tlzallen@163.com>
2020-07-14 21:36:30 +0800

    通过 implements BeanFactoryPostProcessor 在bean未定义时，修改bean

---
+++ src/main/java/com/aaa/javabase/service/OrderService.java
@@ -0,0 +1,16 @@
+package com.aaa.javabase.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author liuzhen.tian
+ * @version 1.0    2020/7/14 21:03
+ */
+@Component
+@Slf4j
+public class OrderService {
+    public OrderService() {
+        log.info("OrderService begin ...");
+    }
+}
---
+++ src/main/java/com/aaa/javabase/service/UserService.java
@@ -0,0 +1,15 @@
+package com.aaa.javabase.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author liuzhen.tian
+ * @version 1.0    2020/7/14 21:01
+ */
+@Slf4j
+public class UserService {
+
+    public UserService() {
+        log.info("UserService begin ...");
+    }
+}
