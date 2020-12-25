8204867b186fc885e0ffe95e1f4051fd8f145921
tianliuzhen <tlzallen@163.com>
2020-12-25 14:06:53 +0800

    小记  ParallelStreams 并行流计算

---
+++ src/main/java/com/aaa/javabase/jdk8/stream/groupby/Fruit.java
@@ -0,0 +1,20 @@
+package com.aaa.javabase.jdk8.stream.groupby;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author liuzhen.tian
+ * @version 1.0 Fruit.java  2020/12/25 13:03
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Fruit {
+    String name;
+    Double price;
+    Double weight;
+    Double sum;
+}
