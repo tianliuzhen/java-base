0c38fa382e0eac6298993b1a327817994246f24e
tianliuzhen <tlzallen@163.com>
2021-04-20 22:55:30 +0800

    registerChooser 改造

--- src/main/java/com/aaa/javabase/pattern/behavior/strategy/InspectionSolverChooser.java
+++ src/main/java/com/aaa/javabase/pattern/behavior/strategy/InspectionSolverChooser.java
@@ -1,6 +1,7 @@
 package com.aaa.javabase.pattern.behavior.strategy;
 
 import com.aaa.javabase.pattern.behavior.strategy.constant.InspectionEnum;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 /**
  * @author liuzhen.tian
@@ -29,12 +31,27 @@ public class InspectionSolverChooser implements ApplicationContextAware {
 
     @PostConstruct
     private void register() {
-        Map<String, InspectionSolver> solverMap  = context.getBeansOfType(InspectionSolver.class);
-        for (InspectionSolver value : solverMap .values()) {
-            chooseMap.put(value.supports(), value);
-             log.info( "任务 {} 处理器 {}注册成功", new Object[]{value.supports(), value});
+        registerChooser(chooseMap, InspectionSolver.class, InspectionSolver::supports);
+    }
+
+    /**
+     * 注册
+     *
+     * @param chooseMap   缓存 map
+     * @param solverClass 解析器
+     * @param getKeyFun   获取key函数
+     * @param <K>         key
+     * @param <V>         value
+     */
+    public <K, V> void registerChooser(Map<K, V> chooseMap, Class<V> solverClass, Function<V, K> getKeyFun) {
+        Map<String, V> solverMap = context.getBeansOfType(solverClass);
+        for (V value : solverMap.values()) {
+            K key = getKeyFun.apply(value);
+            chooseMap.put(key, value);
+            log.info("任务 {} 处理器 {}注册成功", new Object[]{key, value});
         }
     }
+
 
     public InspectionSolver choose(InspectionEnum type) {
         return chooseMap.get(type);
