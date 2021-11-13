8650e4f9e28afcadc4f7406da509777a95edf6f7
tianliuzhen <tlzallen@163.com>
2021-11-13 17:44:01 +0800

    注释 ...

--- src/main/java/com/aaa/javabase/base/protecteds/demo1/p1/Test1.java
+++ src/main/java/com/aaa/javabase/base/protecteds/demo1/p1/Test1.java
@@ -4,6 +4,8 @@ package com.aaa.javabase.base.protecteds.demo1.p1;
 import com.aaa.javabase.base.protecteds.demo1.p2.Son2;
 
 /**
+ * 子类与基类在同一包中：被声明为 protected 的变量、方法和构造器能被同一个包中的任何其他类访问；
+ *
  * @author liuzhen.tian
  * @version 1.0 Test1.java  2020/10/12 14:36
  */
--- src/main/java/com/aaa/javabase/base/protecteds/demo1/p2/Test2.java
+++ src/main/java/com/aaa/javabase/base/protecteds/demo1/p2/Test2.java
@@ -5,6 +5,8 @@ import com.aaa.javabase.base.protecteds.demo1.p1.Father1;
 import com.aaa.javabase.base.protecteds.demo1.p1.Son1;
 
 /**
+ * 子类与基类不在同一包中：那么在子类中，子类实例可以访问其从基类继承而来的 protected 方法，而不能访问基类实例的protected方法。
+ *
  * @author liuzhen.tian
  * @version 1.0 Test1.java  2020/10/12 14:36
  */
