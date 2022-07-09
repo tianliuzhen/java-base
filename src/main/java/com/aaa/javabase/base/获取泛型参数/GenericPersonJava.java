46a38d35ee6393b59dc8626d011826ce7f9c3931
tianliuzhen <tlzallen@163.com>
2022-07-09 21:22:48 +0800

    java 泛型擦除理解

---
+++ src/main/java/com/aaa/javabase/base/获取泛型参数/GenericPersonJava.java
@@ -0,0 +1,11 @@
+package com.aaa.javabase.base.获取泛型参数;
+
+/**
+ * @author liuzhen.tian
+ * @version 1.0 GenericPersonJava.java  2022/7/7 22:12
+ */
+public class GenericPersonJava<T> {
+    GenericPersonJava(){
+        // T t = new T();
+    }
+}
