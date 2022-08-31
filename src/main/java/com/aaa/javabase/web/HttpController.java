fc667a2bea86fcb94172b7daaa9a8939e662e0d5
tianliuzhen <tlzallen@163.com>
2022-08-31 20:43:47 +0800

    springboot如何接收application/x-www-form-urlencoded类型的

---
+++ src/main/java/com/aaa/javabase/web/HttpController.java
@@ -0,0 +1,33 @@
+package com.aaa.javabase.web;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+/**
+ * @author liuzhen.tian
+ * @version 1.0 httpController.java  2022/8/31 20:39
+ */
+@RestController
+@RequestMapping(value = "/httpController")
+public class HttpController {
+
+    /**
+     * ==========================  springboot如何接收application/x-www-form-urlencoded类型的
+     * 注意：请求类型只能是 post 否则无法接收
+     */
+    @PostMapping("/req1")
+    public void detailByParam(@RequestParam Integer id, @RequestParam(value = "name") String name ) {
+        System.out.println(">>> id=" + id + ",name=" + name);
+    }
+
+    @PostMapping("/req2")
+    public void detailByParam(@RequestParam Map<String, String> params) {
+        System.out.println(">>>id=" + params.get("id") + ",name=" + params.get("name"));
+    }
+
+    @PostMapping("/detailByParam")
+    public void detailByParam(@RequestBody String params) {
+        System.out.println(">>>"+params);
+    }
+}
