b8d5c3bf671b4f17eb5144b7a8748b9d09a5d061
tianliuzhen <tlzallen@163.com>
2022-10-28 21:12:10 +0800

    ConditionalOnProperty 记录

--- src/main/java/com/aaa/javabase/config/ConfigurationSupport.java
+++ src/main/java/com/aaa/javabase/config/ConfigurationSupport.java
@@ -6,16 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * name 不存在
- *      matchIfMissing=true，会加载
- *      matchIfMissing=false，不会加载
+ *  matchIfMissing=true
+ *      name 不存在，会加载
+ *      name 存在，匹配havingValue ，会加载
+ *      name 存在，不匹配havingValue ，不会加载
  *
- * name 存在
- *     匹配 havingValue， matchIfMissing=true，会加载
- *     不匹配 havingValue， matchIfMissing=true，不会加载
- *
- *     匹配 havingValue， matchIfMissing=false，会加载
- *     不匹配 havingValue， matchIfMissing=false，不会加载
+ *  matchIfMissing=false
+ *      name 不存在，不会加载
+ *      name 存在，匹配havingValue ，会加载
+ *      name 存在，不匹配havingValue ，不会加载
  *
  * @author liuzhen.tian
  * @version 1.0 ConfigurationSupport.java  2020/12/13 22:46
