9bf911bdfc8bc4dd7995e2e0ac7cd6dc297e8ef2
tianliuzhen <tlzallen@163.com>
2023-01-28 22:25:09 +0800

    记录阿里云oss测试

---
+++ src/test/java/com/aaa/javabase/util/TestAliyunOss.java
@@ -0,0 +1,194 @@
+package com.aaa.javabase.util;
+
+/**
+ * @author liuzhen.tian
+ * @version 1.0 TestAliyunOss.java  2023/1/28 21:53
+ */
+
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.CannedAccessControlList;
+import com.aliyun.oss.model.ObjectMetadata;
+import com.aliyun.oss.model.PutObjectRequest;
+
+import java.io.*;
+
+/**
+ * AccessKey ID: LTAI5tQQAPKkbekzKgDvqLp8
+ * AccessKey Secret: nKDyFqsVU0qAjgASnSpuHazsaodrFZ
+ * Endpoint：oss-cn-hangzhou.aliyuncs.com
+ * Bucket：tls-oss.oss-cn-hangzhou.aliyuncs.com
+ *
+ * @author tlz
+ * @version $Id: TestAliyunOss.java,v 0.1 2022年12月13日  6:27 PM:17 Exp $
+ */
+public class TestAliyunOss {
+    public static void main(String[] args) throws Exception {
+        testFile();
+        // testUpLoadJpg();
+    }
+
+    private static void testFile() {
+        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+        String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5tQQAPKkbekzKgDvqLp8";
+        String accessKeySecret = "nKDyFqsVU0qAjgASnSpuHazsaodrFZ";
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "tls-oss";
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "demo/test2-3.txt";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+            // 填写字符串。
+            String content = "Hello OSS 32";
+
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, new ByteArrayInputStream(content.getBytes()));
+
+            // 如果需要上传时设置存储类型和访问权限，请参考以下示例代码。
+            // ObjectMetadata metadata = new ObjectMetadata();
+            // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+            // metadata.setObjectAcl(CannedAccessControlList.Private);
+            // putObjectRequest.setMetadata(metadata);
+
+            // 上传字符串。
+            ossClient.putObject(putObjectRequest);
+            // 上传后也能修改文件权限
+            ossClient.setObjectAcl(bucketName, objectName, CannedAccessControlList.Private);
+
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+        System.out.println("https://tls-oss.oss-cn-hangzhou.aliyuncs.com/" + objectName);
+    }
+
+    private static void testUpLoadJpg() {
+        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+        String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5tQQAPKkbekzKgDvqLp8";
+        String accessKeySecret = "nKDyFqsVU0qAjgASnSpuHazsaodrFZ";
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "tls-oss";
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String fileName = "demo/test2.jpg";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+
+            // 获取图片url
+            byte[] bytes = fileToByteArray("/Users/tlz/Documents/钉钉下载/test.png");
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, fileName, new ByteArrayInputStream(bytes));
+
+            // 如果需要上传时设置存储类型和访问权限，请参考以下示例代码。
+            ObjectMetadata metadata = new ObjectMetadata();
+            // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+            // metadata.setObjectAcl(CannedAccessControlList.Private);
+
+            metadata.setContentType(getcontentType(fileName.substring(fileName.lastIndexOf("."))));
+            putObjectRequest.setMetadata(metadata);
+            // 上传字符串。
+            ossClient.putObject(putObjectRequest);
+
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+        }
+    }
+
+    public static String getcontentType(String FilenameExtension) {
+        if (FilenameExtension.equalsIgnoreCase(".bmp")) {
+            return "image/bmp";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".gif")) {
+            return "image/gif";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".jpeg") ||
+                FilenameExtension.equalsIgnoreCase(".jpg") ||
+                FilenameExtension.equalsIgnoreCase(".png")) {
+            return "image/jpg";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".html")) {
+            return "text/html";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".txt")) {
+            return "text/plain";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".vsd")) {
+            return "application/vnd.visio";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".pptx") ||
+                FilenameExtension.equalsIgnoreCase(".ppt")) {
+            return "application/vnd.ms-powerpoint";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".docx") ||
+                FilenameExtension.equalsIgnoreCase(".doc")) {
+            return "application/msword";
+        }
+        if (FilenameExtension.equalsIgnoreCase(".xml")) {
+            return "text/xml";
+        }
+        return "image/jpg";
+    }
+
+    public static byte[] fileToByteArray(String filePath) {
+        //1.创建源yu目的地
+        File file = new File(filePath);
+        byte[] ds = null;
+        //选择流
+        InputStream zp = null;
+        ByteArrayOutputStream boos = null;
+        boos = new ByteArrayOutputStream();
+        try {
+            zp = new FileInputStream(file);
+            byte[] frush = new byte[1024];//1024表示1k为一段
+            int len = -1;
+            while ((len = zp.read(frush)) != -1) {
+                boos.write(frush, 0, len);//写出到字节数组中
+
+            }
+            boos.flush();
+            return boos.toByteArray();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            if (zp != null) {
+                try {
+                    zp.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+        }
+        return null;
+    }
+}
