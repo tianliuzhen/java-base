24ade66616350a7cd56445473153b2699a09a302
tianliuzhen <tlzallen@163.com>
2023-03-29 21:44:37 +0800

    记录 JSON.toJavaObject

--- src/test/java/com/aaa/javabase/fastjson/TestJsonPath.java
+++ src/test/java/com/aaa/javabase/fastjson/TestJsonPath.java
@@ -3,6 +3,7 @@ package com.aaa.javabase.fastjson;
 import com.aaa.javabase.domain.ConfigVarModel;
 import com.aaa.javabase.util.FileUtil;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.JSONPath;
 
@@ -22,6 +23,12 @@ public class TestJsonPath {
         // 测试取对象
         JSONObject byFilePath = FileUtil.getByFilePath("json/other.json");
         JSONPath.eval(byFilePath, "$.result");
+        System.out.println();
+
+        // JavaObject 转 泛型：
+        String str = JSONObject.toJSONString(configVars);
+        JSONArray strArray = JSONArray.parseArray(str);
+        ConfigVarModel configVarModel = JSON.toJavaObject(strArray.getJSONObject(0), ConfigVarModel.class);
         System.out.println();
     }
 }
