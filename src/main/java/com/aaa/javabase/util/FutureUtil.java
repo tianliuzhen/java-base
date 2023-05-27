604460124cb872322bbbf27b120b2844d54cffb9
tianliuzhen <tlzallen@163.com>
2023-05-27 13:42:22 +0800

    CompletableFuture.runAsync 默认核心线程的坑

--- src/main/java/com/aaa/javabase/util/FutureUtil.java
+++ src/main/java/com/aaa/javabase/util/FutureUtil.java
@@ -33,11 +33,7 @@ public class FutureUtil {
         try {
             // 1、构建Future线程
             for (Supplier<T> supplier : suppliers) {
-                if (null == poolExecutor) {
-                    futureList.add(CompletableFuture.supplyAsync(supplier));
-                } else {
-                    futureList.add(CompletableFuture.supplyAsync(supplier, poolExecutor));
-                }
+                futureList.add(CompletableFuture.supplyAsync(supplier, poolExecutor));
             }
 
             // 2、阻塞线程直到所有线程完成
@@ -70,12 +66,34 @@ public class FutureUtil {
         try {
             // 1、构建Future线程
             for (Runnable runnable : runnableList) {
-                if (null == poolExecutor) {
-                    futureList.add(CompletableFuture.runAsync(runnable));
-                } else {
-                    futureList.add(CompletableFuture.runAsync(runnable, poolExecutor));
-                }
+                futureList.add(CompletableFuture.runAsync(runnable, poolExecutor));
+            }
 
+            // 2、阻塞线程直到所有线程完成
+            CompletableFuture[] completableFutures = futureList.toArray(new CompletableFuture[futureList.size()]);
+            CompletableFuture.allOf(completableFutures).get();
+        } catch (Exception e) {
+            log.error("[doRunAsync is error]", e);
+        }
+        return results;
+    }
+
+    /**
+     * 并发执行，带返回值
+     * 谨慎使用，此方法会默认的核心线程是 n-1 = Runtime.getRuntime().availableProcessors() - 1
+     * java.util.concurrent.ForkJoinPool#makeCommonPool()
+     *
+     * @param runnableList 函数
+     * @param <T>          泛型返回值
+     * @return List<T>
+     */
+    public static <T> List<T> doRunAsync(List<Runnable> runnableList) {
+        List<T> results = new ArrayList<>();
+        List<CompletableFuture<Void>> futureList = new ArrayList<>();
+        try {
+            // 1、构建Future线程
+            for (Runnable runnable : runnableList) {
+                futureList.add(CompletableFuture.runAsync(runnable));
             }
 
             // 2、阻塞线程直到所有线程完成
@@ -91,7 +109,32 @@ public class FutureUtil {
         // testOne();
 
         // 会有线程安全问题
-        testError();
+        // testError();
+
+        // 这里有坑，核心线程就n-1个，超过就会阻塞
+        testDoRunAsync();
+    }
+
+    private static void testDoRunAsync() {
+        List<Runnable> runnableList = new ArrayList<>();
+        // 这里超过7个就会阻塞了
+        for (int i = 0; i < 8; i++) {
+            runnableList.add(() -> {
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+        }
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        List<Object> objects = doRunAsync(runnableList);
+        stopWatch.stop();
+        double totalTimeSeconds = stopWatch.getTotalTimeSeconds();
+
+        System.out.println(totalTimeSeconds);
     }
 
     /**
@@ -138,10 +181,10 @@ public class FutureUtil {
             });
         }
         // 并发执行会影响 result
-        FutureUtil.doRunAsync(runnables,null);
+        FutureUtil.doRunAsync(runnables, null);
     }
 
-    public static void testError(HashMap<String, Object> result){
+    public static void testError(HashMap<String, Object> result) {
         System.out.println(result);
     }
 }
