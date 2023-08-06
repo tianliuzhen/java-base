5a2404cf4f6b69c6b0f55934520ac2bc9cdca3da
tianliuzhen <tlzallen@163.com>
2023-08-06 17:03:24 +0800

    基于builder实现重试RetryTemplate

--- src/main/java/com/aaa/javabase/common/retry/RetryTemplate.java
+++ src/main/java/com/aaa/javabase/common/retry/RetryTemplate.java
@@ -1,78 +1,70 @@
 package com.aaa.javabase.common.retry;
 
+import lombok.Builder;
+import lombok.Data;
 import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.log4j.Log4j2;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author liuzhen.tian
  * @version 1.0 RetryTemplate.java  2020/8/17 15:53
  */
-@Slf4j
-public abstract class RetryTemplate {
+@Log4j2
+@Data
+@Builder
+public class RetryTemplate {
 
-    private static final int DEFAULT_RETRY_TIME = 1;
-    private int retryTime = DEFAULT_RETRY_TIME;
+    private int retryTime = 1;
     /**
-     * 重试的睡眠时间
+     * 重试的睡眠时间，默认是ms
      */
     private int sleepTime = 0;
 
-    public int getSleepTime() {
-        return sleepTime;
-    }
+    /**
+     * 业务方法
+     * java.util.concurrent.Callable
+     * java.util.function.Supplier
+     */
+    private Callable bizTask;
+    /**
+     * 时间类型
+     */
+    private TimeUnit timeUnit = TimeUnit.MILLISECONDS;
 
-    public RetryTemplate setSleepTime(int sleepTime) {
-        if(sleepTime < 0) {
-            throw new IllegalArgumentException("sleepTime should equal or bigger than 0");
-        }
-        this.sleepTime = sleepTime;
-        return this;
-    }
-
-    public int getRetryTime() {
-        return retryTime;
-    }
-
-    public RetryTemplate setRetryTime(int retryTime) {
-        if (retryTime <= 0) {
-            throw new IllegalArgumentException("retryTime should bigger than 0");
-        }
-        this.retryTime = retryTime;
-        return this;
-    }
 
     /**
-     * 重试的业务执行代码
-     * 失败时请抛出一个异常
-     *
-     * todo 确定返回的封装类，根据返回结果的状态来判定是否需要重试
-     *
      * @return
      */
-    protected abstract Object doBiz() throws Exception; //预留一个doBiz方法由业务方来实现，在其中书写需要重试的业务代码，然后执行即可
-
     @SneakyThrows
-    public Object execute() {
+    public Object execute() throws Exception {
         for (int i = 0; i < retryTime; i++) {
             try {
-                return doBiz();
+                return bizTask.call();
             } catch (Exception e) {
-                Thread.sleep(sleepTime);
-                e.printStackTrace();
-
-
+                this.timeUnit.sleep(this.sleepTime);
+                log.warn("RetryTemplate.execute.重试" + (retryTime + 1) + "几次");
+                log.error("RetryTemplate.execute.error", e);
             }
         }
         return null;
     }
 
+    /**
+     * 带线程池执行
+     *
+     * @param executorService
+     * @return
+     */
     public Object submit(ExecutorService executorService) {
         if (executorService == null) {
             throw new IllegalArgumentException("please choose executorService!");
         }
-        return executorService.submit((Callable) () -> execute());
+        return executorService.submit(this::execute);
     }
+
+
 }
