1cf4e1c1938b32d37f8f267cbf790a52142a65ac
tianliuzhen <tlzallen@163.com>
2024-01-03 21:17:39 +0800

    逆变和协变的再次理解

--- src/main/java/com/aaa/javabase/base/list/协变和逆变/TestE.java
+++ src/main/java/com/aaa/javabase/base/list/协变和逆变/TestE.java
@@ -39,7 +39,15 @@ public class TestE {
         List<? extends Number> numbers = new ArrayList<Integer>() {{
             add(1);
         }};
+        /*
+         在Java中，当你声明一个类型为 List<? extends Number> 的变量时，
+         你实际上是在说：“我想要一个列表，它可以包含Number或其任何子类的实例，但我不确定具体是哪一个。” 由于这种不确定性，Java编译器限制了你可以对这个列表做的操作，以防止类型不安全。
+         具体来说，List<? extends Number> 是一个通配符泛型表达式，它表示列表的元素可以是Number或其任何子类。
+         然而，由于你不知道具体是哪个子类（或者是Number本身），
+         你不能向这个列表中添加除了null之外的任何元素。这是因为Java无法保证你添加的元素符合列表实际要求的类型。
+         */
         // numbers.add(123); //编译失败
+
         numbers.get(0);
 
 
@@ -53,6 +61,17 @@ public class TestE {
          * 特性：可以add，也可以get
          */
         List<? super Integer> numbers2 = new ArrayList<Number>();
+        /*
+         * 当你声明一个类型为 List<? super Integer> 的变量时，你实际上是在说：“我想要一个列表，它可以包含Integer或其任何父类的实例
+         * 与List<? extends Number>不同，List<? super Integer> 允许向列表中添加Integer或其父类的实例。
+         * 这是因为在这个上下文中，通配符表示的是“Integer”或其父类，而不是“Number”或其子类
+         * 当你声明 List<? super Integer> numbers2 时，你告诉编译器你有一个列表，其元素类型可以是Integer或其父类。
+         * 因此，你可以将整数（Integer的实例）添加到这个列表中，因为整数是Integer的实例，并且Integer是Number的子类。
+         * 所以，对于 List<? super Integer> numbers2 = new ArrayList<Number>(); 这段代码：
+         * 虽然它声明为 ArrayList<Number>，但由于上下文的限制（List<? super Integer>），
+         * 它实际上是一个可以存储Integer或其父类的列表。
+         * 你可以将整数（作为Integer实例）添加到这个列表中，因为整数是Integer的一个实例，而Integer是Number的子类。
+         */
         numbers2.add(123);
         // 编译器不能确定容器中是什么类型的元素
         // Number number2 = numbers2.get(0); // 编译失败
