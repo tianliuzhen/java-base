54b1879a24f2fd69ffba29c7eb7d053ba0e1cdbd
tianliuzhen <tlzallen@163.com>
2024-01-03 21:03:43 +0800

    记录 list多字段排序

---
+++ src/main/java/com/aaa/javabase/jdk8/sort/list/TestStreamSortByMany.java
@@ -0,0 +1,30 @@
+package com.aaa.javabase.jdk8.sort.list;
+
+import com.aaa.javabase.jdk8.sort.MonthDataDto;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author liuzhen.tian
+ * @version 1.0 TestStreamSortByMany.java  2024/1/3 20:58
+ */
+public class TestStreamSortByMany {
+    public static void main(String[] args) {
+        List<MonthDataDto> list = new ArrayList<>();
+        list.add(new MonthDataDto("d", "1", "937400", "1200.11", "", ""));
+        list.add(new MonthDataDto("a", "1", "937400", "1200.11", "", ""));
+        list.add(new MonthDataDto("c", "1", "937400", "1200.11", "", ""));
+        list.add(new MonthDataDto("c1", "3", "937400", "1200.11", "", ""));
+        list.add(new MonthDataDto("c2", "3", "937400", "1200.11", "", ""));
+        list.add(new MonthDataDto("b", "2", "937400", "1200.11", "", ""));
+
+        List<MonthDataDto> collect = list.stream().sorted(
+                Comparator.comparing(MonthDataDto::getMoneyMonth)
+                        .thenComparing(MonthDataDto::getStoreName))
+                .collect(Collectors.toList());
+        System.out.println();
+    }
+}
