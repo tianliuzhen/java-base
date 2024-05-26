fc2483aebd2ea517eb7aed1717c66d6a7ed67cde
tianliuzhen <tlzallen@163.com>
2024-05-26 12:25:04 +0800

    InheritableThreadLocal 再次理解

--- src/main/java/com/aaa/javabase/multithreading/threadlocal/inheritableThreadLocal/InheritableThreadLocalTest1.java
+++ src/main/java/com/aaa/javabase/multithreading/threadlocal/inheritableThreadLocal/InheritableThreadLocalTest1.java
@@ -1,8 +1,6 @@
 package com.aaa.javabase.multithreading.threadlocal.inheritableThreadLocal;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author liuzhen.tian
@@ -10,11 +8,25 @@ import java.util.concurrent.TimeUnit;
  */
 public class InheritableThreadLocalTest1 {
     public static ThreadLocal<Integer> threadLocal = new InheritableThreadLocal<>();
+
+    // 把 executorService 换成这个就不会存在下面问题
+    // final static ThreadPoolExecutor executorService = new ThreadPoolExecutor(5, 5,
+    //         1, TimeUnit.SECONDS, new ArrayBlockingQueue<>(10));
+
+    /**
+     * 下面所有的业务测试，都是属于内存泄漏的情况（每次使用完没有 clear ThreadLocal）
+     * 因为 Executors.newFixedThreadPool(1)，创建的线程都是常驻线程，不会销毁。
+     * 这就会导致一次父线程 threadLocal.set(2); 之后，
+     * 因为子线程已经创建，再次执行时 parent.inheritableThreadLocals 还是第一次 new Thread()是，从父类copy的
+     * 所以不会再次执行new Thread()，
+     * 所以第二次 executorService.submit 里面的第一次输出 是null
+     */
     public static ExecutorService executorService = Executors.newFixedThreadPool(1);
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("主线程开启");
         threadLocal.set(1);
+
 
         executorService.submit(() -> {
             System.out.println("子线程读取本地变量：" + threadLocal.get());
@@ -25,6 +37,9 @@ public class InheritableThreadLocalTest1 {
 
         threadLocal.set(2);
 
+        /*
+         *  因为executorService
+         */
         executorService.submit(() -> {
             System.out.println("子线程读取本地变量：" + threadLocal.get());
             threadLocal.set(3);
--- src/main/java/com/aaa/javabase/multithreading/threadlocal/inheritableThreadLocal/MyInheritableThreadLocal.java
+++ src/main/java/com/aaa/javabase/multithreading/threadlocal/inheritableThreadLocal/MyInheritableThreadLocal.java
@@ -5,13 +5,18 @@ import com.alibaba.fastjson.JSONObject;
 
 
 /**
+ * 在子线程返回主线程的ThreadLocal值中，做隔离，本质就是修改ThreadLocal value中的引用
+ * java.lang.ThreadLocal.ThreadLocalMap#ThreadLocalMap(java.lang.ThreadLocal.ThreadLocalMap)
+ *
  * @author liuzhen.tian
  * @version 1.0 MyInheritableThreadLocal.java  2020/10/25 0:54
  */
 public class MyInheritableThreadLocal<T> extends InheritableThreadLocal<T> {
+
+    @Override
     protected T childValue(T parentValue) {
         String s = JSONObject.toJSONString(parentValue);
-        return (T) JSONObject.parseObject(s,parentValue.getClass());
+        return (T) JSONObject.parseObject(s, parentValue.getClass());
     }
 }
 
