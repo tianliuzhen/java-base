433decbdd076e0fb66d289cb2e7444a63d42c313
tianliuzhen <tlzallen@163.com>
2024-06-02 13:54:18 +0800

    TransmittableThreadLocal 主要是解决什么问题

--- src/main/java/com/aaa/javabase/multithreading/threadlocal/inheritableThreadLocal/TestTransmittableThreadLocal.java
+++ src/main/java/com/aaa/javabase/multithreading/threadlocal/inheritableThreadLocal/TestTransmittableThreadLocal.java
@@ -5,13 +5,26 @@ import com.alibaba.ttl.TtlRunnable;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
+ * InheritableThreadLocal 虽然可以解决线程池父子线程传递变量的问题，
+ * 但是不能解决常驻线程的问题
+ *
  * @author liuzhen.tian
  * @version 1.0 TestTransmittableThreadLocal.java  2024/5/26 12:55
+ * @see com.aaa.javabase.multithreading.threadlocal.inheritableThreadLocal.InheritableThreadLocalTest1
+ * 因只有一个线程1，线程激活后，再次set ，子线程无法感知
+ * 而，TransmittableThreadLocal 的核心作用，就是为了解决这个问题
+ * <p>
+ * 我在工作中一般不使用TransmittableThreadLocal，
+ * 但原理都是类似的，每次线程池提交之前，会包装一下 runnable 把主线程ThreadLocal变量传递过来
  */
 public class TestTransmittableThreadLocal {
+    static TransmittableThreadLocal<Map<String, String>> USER_CONTEXT = new TransmittableThreadLocal<>();
+    static ExecutorService executorService = Executors.newFixedThreadPool(1);
 
     /**
      * https://blog.csdn.net/qq_18300037/article/details/128776124
@@ -20,25 +33,27 @@ public class TestTransmittableThreadLocal {
      * @param args
      */
     public static void main(String[] args) {
-        // 原理
-        // testAtomicReference();
-
-
         //使用TTL
-        TransmittableThreadLocal<Map<String, String>> USER_CONTEXT = new TransmittableThreadLocal<>();
-
         USER_CONTEXT.set(new HashMap<String, String>() {{
             put("name", "tom");
         }});
 
-        //将普通的Runnable包装成TtlRunnable
-        TtlRunnable ttlRunnable = TtlRunnable.get(() -> {
+        executorService.submit(TtlRunnable.get(() -> {
             System.out.println(USER_CONTEXT.get().get("name"));
-        });
-        new Thread(ttlRunnable).start();
+        }));
 
+        //使用TTL
+        USER_CONTEXT.set(new HashMap<String, String>() {{
+            put("name", "allen");
+        }});
+
+        executorService.submit(TtlRunnable.get(() -> {
+            System.out.println(USER_CONTEXT.get().get("name"));
+        }));
     }
 
+    // 原理
+    // testAtomicReference();
     private static void testAtomicReference() {
         AtomicReference<Object> capturedRef = new AtomicReference<>();
         capturedRef.set(new HashMap<String, String>() {{
