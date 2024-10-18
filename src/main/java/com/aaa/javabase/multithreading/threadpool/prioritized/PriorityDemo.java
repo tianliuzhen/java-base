58c31f527d221cc5215268fe48b1c99e9a96ba7f
tianliuzhen <tlzallen@163.com>
2024-10-18 22:43:31 +0800

    注释

--- src/main/java/com/aaa/javabase/multithreading/threadpool/prioritized/PriorityDemo.java
+++ src/main/java/com/aaa/javabase/multithreading/threadpool/prioritized/PriorityDemo.java
@@ -21,23 +21,26 @@ public class PriorityDemo {
 
         ThreadPoolExecutor executor = new ThreadPoolExecutor(
                 1,
-                2,
+                10,
                 Long.MAX_VALUE, /* timeout */
                 TimeUnit.NANOSECONDS,
                 new PriorityBlockingQueue<Runnable>(),
                 new ThreadPoolExecutor.DiscardOldestPolicy());
 
         PrioritizedRunnable p1 = new PrioritizedRunnable(1, "name1-1");
-        PrioritizedRunnable p2 = new PrioritizedRunnable(6, "name4-2");
-        PrioritizedRunnable p3 = new PrioritizedRunnable(999, "name5-3");
-        PrioritizedRunnable p4 = new PrioritizedRunnable(9, "name3-4");
-        PrioritizedRunnable p5 = new PrioritizedRunnable(89, "name2-5");
 
-        executor.execute(p4);
+        PrioritizedRunnable p2 = new PrioritizedRunnable(33, "name4-2");
+        PrioritizedRunnable p3 = new PrioritizedRunnable(6, "name4-2");
+        PrioritizedRunnable p4 = new PrioritizedRunnable(9, "name5-3");
+
+        // 这个线程先填满线程池，后面线程进来就会按顺序执行
         executor.execute(p1);
+
+        // 按顺序执行
         executor.execute(p2);
         executor.execute(p3);
-        executor.execute(p5);
+        executor.execute(p4);
+
         log.info("submit 5 Runnable");
         // Thread.sleep(30*1000);
         executor.shutdown();
