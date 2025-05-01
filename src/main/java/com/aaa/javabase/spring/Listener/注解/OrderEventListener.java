0383a105da09aea52dc0d88a74ed8e7fd4951a33
tianliuzhen <tlzallen@163.com>
2025-05-01 19:10:24 +0800

    @EventListener 注解

--- src/main/java/com/aaa/javabase/spring/Listener/注解/OrderEventListener.java
+++ src/main/java/com/aaa/javabase/spring/Listener/注解/OrderEventListener.java
@@ -2,6 +2,7 @@ package com.aaa.javabase.spring.Listener.注解;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,10 +13,25 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class OrderEventListener  implements InitializingBean {
+
+    @Order(1)
     @EventListener
-    public void handleOrderEvent(OrderEvent event) {
-        System.out.println("我监听到了handleOrderEvent发布的message为:" + event.getMsg());
+    public void handleOrderEvent1(OrderEvent event) {
+        System.out.println("我监听到了handleOrderEvent1发布的message为:" + event.getMsg());
     }
+
+    @Order(0)
+    @EventListener
+    public void handleOrderEvent0(OrderEvent event) {
+        System.out.println("我监听到了handleOrderEvent0发布的message为:" + event.getMsg());
+    }
+
+    @Order(2)
+    @EventListener
+    public void handleOrderEvent2(OrderEvent event) {
+        System.out.println("我监听到了handleOrderEvent2发布的message为:" + event.getMsg());
+    }
+
 
     @Override
     public void afterPropertiesSet() throws Exception {
